a8e0ab76c615eb7d70231a2340985c2fda9586df
Bryndis Sturludottir <bryndis@netapp.com>
2020-10-17 16:51:52 +0000

    derp me

--- client/src/components/informationbar/Notification.js
+++ client/src/components/informationbar/Notification.js
@@ -3,9 +3,10 @@ import React, { Fragment } from 'react';
 const Notification = () => {
   return (
     <Fragment>
-        <div className="alert alert-dark" role="alert">
-          New message from James Bond! <a href="#" class="alert-link">Click to view</a>
-          <button type="button" class="close" aria-label="Close">
+        <div className="alert row alert-dark" role="alert">
+          <p>New message from James Bond!</p>
+          <a href="#" className="alert-link col-md-10">Click to view</a>
+          <button type="button" className="close col-md-2" aria-label="Close">
           <span aria-hidden="true">&times;</span>
           </button>
         </div>
